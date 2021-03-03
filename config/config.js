9260602f43541c0bfb870929c3443878d581d361
purplebabar <lalung.alexandre@gmail.com>
2021-03-03 17:29:44 +0100

    new releases cross browser

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {  
-  var redirections = JSON.parse(localStorage.sshFlashlight).redirections
-  for (let slug in redirections) {
-    var data = document.getElementById('redirections').insertRow()
+  var flashs = JSON.parse(localStorage.sshFlashlight).flashs
+  for (let slug in flashs) {
+    var data = document.getElementById('flashs').insertRow()
     data.insertCell().innerHTML = slug;
-    data.insertCell().innerHTML = redirections[slug];
+    data.insertCell().innerHTML = flashs[slug];
     data.insertCell().innerHTML = '<button id="'+slug+'" class="delete button button-clear">Delete</button>';
   }
   
@@ -11,26 +11,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
     item.addEventListener('click', (event) => {
       var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
       
-      delete sshFlashlight.redirections[event.target.id]
+      delete sshFlashlight.flashs[event.target.id]
       localStorage.sshFlashlight = JSON.stringify(sshFlashlight)
       
       event.target.parentElement.parentElement.remove()
     })
   }
   
-  var form = document.getElementById('config');
+  var form = document.getElementById('configf');
   form.addEventListener('submit', function(e) {
       e.preventDefault(); // don't submit
       var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
 
-      sshFlashlight.redirections[document.getElementById('slug').value] = document.getElementById('redirect').value
+      sshFlashlight.flashs[document.getElementById('slug').value] = document.getElementById('flash').value
       
-      var data = document.getElementById('redirections').insertRow()
+      var data = document.getElementById('flashs').insertRow()
       data.insertCell().innerHTML = document.getElementById('slug').value;
-      data.insertCell().innerHTML = document.getElementById('redirect').value;
+      data.insertCell().innerHTML = document.getElementById('flash').value;
       data.insertCell().innerHTML = '<button id="'+slug+'" class="delete button button-clear">Delete</button>';
       
       localStorage.sshFlashlight = JSON.stringify(sshFlashlight)
       form.reset()
   })
 });
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var redirections = JSON.parse(localStorage.sshFlashlight).redirections
+  for (let host in redirections) {
+    var data = document.getElementById('redirections').insertRow()
+    data.insertCell().innerHTML = host;
+    data.insertCell().innerHTML = redirections[host];
+    data.insertCell().innerHTML = '<button id="' + host + '" class="delete button button-clear">Delete</button>';
+  }
+
+  for (let item of document.getElementsByClassName("delete")) {
+    item.addEventListener('click', (event) => {
+      var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
+
+      delete sshFlashlight.redirections[event.target.id]
+      localStorage.sshFlashlight = JSON.stringify(sshFlashlight)
+
+      event.target.parentElement.parentElement.remove()
+    })
+  }
+
+  var form = document.getElementById('configr');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault(); // don't submit
+    var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
+
+    sshFlashlight.redirections[document.getElementById('host').value] = document.getElementById('redirect').value
+
+    var data = document.getElementById('redirections').insertRow()
+    data.insertCell().innerHTML = document.getElementById('host').value;
+    data.insertCell().innerHTML = document.getElementById('redirect').value;
+    data.insertCell().innerHTML = '<button id="' + host + '" class="delete button button-clear">Delete</button>';
+
+    localStorage.sshFlashlight = JSON.stringify(sshFlashlight)
+    form.reset()
+  })
+});
+
--- flashlight.js
+++ flashlight.js
@@ -1,29 +1,35 @@
 var active = false;
 
 function flashlight(info) {
-
-  if (info.requestId == localStorage.sshFlashlightHandled) {
+  if (info.requestId == localStorage.sshFlashlightHandled && info.url == localStorage.sshFlashlightHandledUrl) {
     return
-  }  
+  }
 
-  var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
   var urlMatch = info.url.match(/^(?:http)s?:\/\/([\w][^\/=\s]+)\/?|(^w{3}[\.\w][^\/\=\s]{2,})\/?/gi)
   var host = urlMatch ? urlMatch[0] : null 
   var url = info.url
+  if (info.originUrl) {
+    var originMatch = info.originUrl.match(/^(?:http)s?:\/\/([\w][^\/=\s]+)\/?|(^w{3}[\.\w][^\/\=\s]{2,})\/?/gi)
+    var originHost = originMatch ? originMatch[0] : null
+    info.originUrl = originHost
+  }
+  var initiator = info.initiator ? info.initiator : info.originUrl
 
-  for (let slug in sshFlashlight.redirections) {
-    if (host.includes(slug)) {
-      localStorage.sshFlashlightHandled = info.requestId
-      return { redirectUrl : url.replace(/^(?:http)s?:\/\/([\w][^\/=\s]+)\/?|(^w{3}[\.\w][^\/\=\s]{2,})\/?/gi, sshFlashlight.redirections[slug] + '/' ) }
-    }
+  // Redirections part
+  var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
+  if (Object.keys(sshFlashlight.redirections).includes(host)) {
+    localStorage.sshFlashlightHandled = info.requestId
+    localStorage.sshFlashlightHandledUrl = info.url
+    return { redirectUrl: sshFlashlight.redirections[host] }
   }
 
-  if (info.initiator) {
-    var isSameHost = host.includes(info.initiator)
-
+  // Flashlight part
+  if (initiator) {
+    var isSameHost = initiator.includes(host)
     if (host.indexOf("localhost") > 0 || !isSameHost) {
       localStorage.sshFlashlightHandled = info.requestId
-      return { redirectUrl : url.replace(/^(?:http)s?:\/\/([\w][^\/=\s]+)\/?|(^w{3}[\.\w][^\/\=\s]{2,})\/?/gi, info.initiator + '/' ) }
+      localStorage.sshFlashlightHandledUrl = info.url
+      return { redirectUrl: url.replace(/^(?:http)s?:\/\/([\w][^\/=\s]+)\/?|(^w{3}[\.\w][^\/\=\s]{2,})\/?/gi, initiator + (info.originUrl ? "" : "/") ) }
     }
   }
 }
@@ -32,7 +38,7 @@ function flashlight(info) {
 chrome.browserAction.setIcon({path:"inactive.png"});
 if (!localStorage.sshFlashlight) {
   localStorage.sshFlashlight = JSON.stringify({
-    "redirections": {"example": "https://example.com"}
+    "flashs": { "example": "https://example.com" }, "redirections": { "https://example.com/": "https://test.com/" }
   })
 }
 
@@ -40,14 +46,13 @@ if (!localStorage.sshFlashlight) {
 chrome.browserAction.onClicked.addListener(function(tab) {
     active = !active;
     if (active) {
-        
         chrome.browserAction.setIcon({path:"active.png"})
         var types = ["main_frame"]
-        var urls = ["*://localhost:*/*"]
-        
+        var urls = ["*://localhost/*"]
+
         var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
-        for (let slug in sshFlashlight.redirections) {
-            urls.push("*://"+slug+":*/*")
+        for (let redirect in sshFlashlight.redirections) {
+          urls.push(redirect)
         }
         
         chrome.webRequest.onBeforeRequest.addListener(
@@ -61,10 +66,36 @@ chrome.browserAction.onClicked.addListener(function(tab) {
           ["blocking"]
         );
     }else{
-        chrome.browserAction.setIcon({path:"inactive.png"});
-        chrome.webRequest.onBeforeRequest.removeListener(
-          flashlight
-        );
+      chrome.browserAction.setIcon({path:"inactive.png"});
+      chrome.webRequest.onBeforeRequest.removeListener(
+        flashlight
+      );
     }
 
 });
+
+// This event is fired each time the user updates the text in the omnibox,
+// as long as the extension's keyword mode is still active.
+chrome.omnibox.onInputChanged.addListener(
+  function (text, suggest) {
+
+    var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
+    var filtered = Object.keys(sshFlashlight.flashs).filter(element => element.includes(text));
+
+    suggest(
+      Array.from(filtered, slug => {
+        return { content: slug, description: sshFlashlight.flashs[slug] }
+      })
+    );
+  }
+);
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function (text) {
+    var sshFlashlight = JSON.parse(localStorage.sshFlashlight);
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.update(tabs[0].id, { url: sshFlashlight.flashs[text] });
+    });
+  }
+);
